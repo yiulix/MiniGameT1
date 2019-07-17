01a87997822c173eca00043e29fc7ed9362d102e
朱葛 <785930041@qq.com>
2019-07-17 13:07:33 +0800

    人物数据结构+选门scene

---
+++ MiniGame/Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public bool isPlayer = false;
+    public int ID;
+    public int health;
+    public int defense;
+    public int attack;
+
+    public int[] relationships;
+
+    // initialize character attributes by its ID
+    public void Init(int ID)
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+
+    }
+}
+
+public static class CharactersData
+{
+    public static Character c0;
+    public static Character c1;
+    public static Character c2;
+
+}
---
+++ MiniGame/Assets/Scripts/SelectDoorScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDoorScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Decide what content is behind each door
+    void GenerateDoors()
+    {
+
+    }
+
+    public void SelectDoor1()
+    {
+        Debug.Log("SelectDoor1");
+    }
+
+    public void SelectDoor2()
+    {
+
+    }
+
+    public void SelectDoor3()
+    {
+
+    }
+}
